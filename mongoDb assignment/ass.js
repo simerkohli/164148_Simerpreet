2089a0aa5c358bad6c8faa6e579432f559c9d2c3
Simerpreet singh kohli <44640633+simerkohli@users.noreply.github.com>
2019-02-14 16:17:37 +0530

    Mongo Db

---
+++ mongoDb assignment/ass.js
@@ -0,0 +1,53 @@
+
+
+db.zipcodes.aggregate(
+[
+{$match : { city : "ATLANTA" } },
+{$group : { _id : null , total : { $sum : 1 }}}
+]
+);
+
+db.zipcodes.aggregate(
+[
+{$match : { city : "ATLANTA" } },
+{$group : { _id : "Total pop in Atlanta" , total : { $sum : "$pop" } } }
+]
+);
+
+db.zipcodes.aggregate(
+[
+{ $group : { _id : "$state" , total : { $sum : "$pop" } } }
+]
+);
+
+db.zipcodes.aggregate(
+[
+{ $group : { _id : "$state" , total : { $sum : "$pop" } } },
+{ $sort : { total : -1 }},
+{ $limit : 3}
+]
+);
+
+db.zipcodes.aggregate(
+[
+{$group : { _id : {"city": "$city","state":"$state"} , total : { "$sum" : "$pop" } } },
+{ $sort : { total : -1 } },
+{ $limit : 3 }
+]
+);
+
+---Bonus---
+
+db.zipcodes.aggregate(
+[
+{$group : { _id : {"city": "$city","state":"$state"} , avgpop : { "$avg" : "$pop" } } }
+]
+);
+
+db.zipcodes.aggregate(
+[
+{$group : { _id : {"city": "$city","state":"$state"} , avgpop : { "$avg" : "$pop" } } },
+{$sort : { avgpop : -1 } },
+{$limit : 3}
+]
+);
